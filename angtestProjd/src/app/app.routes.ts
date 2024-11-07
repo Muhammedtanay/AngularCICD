d7ed5a5122ea01ec2c88620a3f326143b26cb4ed
MuhammedTANAY <MuhammedTNY@gmail.com>
2024-11-07 16:01:15 +0300

    hakkimizda component ve routes eklendi

--- angtestProjd/src/app/app.routes.ts
+++ angtestProjd/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'hakkimizda', component: hakkimizdaComponent },
+  { path: 'page2', component: Page2Component },
+  { path: 'page3', component: Page3Component },
+  { path: '', redirectTo: '/page1', pathMatch: 'full' }
+];
