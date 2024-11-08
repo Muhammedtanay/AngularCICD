2e881e6cafd1c353740cf75b284927f5096dc6dd
MuhammedTANAY <MuhammedTNY@gmail.com>
2024-11-08 14:33:33 +0300

    Home  route

--- angtestProjd/src/app/app.routes.ts
+++ angtestProjd/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HakkimizdaComponent } from './hakkimizda/hakkimizda.component';
-
+import { AppComponent } from './app.component';
 
 
 export const routes: Routes = [
+  { path: '', component: AppComponent },  
   { path: 'hakkimizda', component: HakkimizdaComponent }
 ];
