814860b3ea1559a55053f0207a5c9e08c730ce54
MuhammedTANAY <MuhammedTNY@gmail.com>
2024-11-11 22:22:24 +0300

    app.module delete

--- angtestProjd/src/app/hakkimizda/hakkimizda.component.ts
+++ angtestProjd/src/app/hakkimizda/hakkimizda.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-hakkimizda',
   templateUrl: './hakkimizda.component.html',
-  styleUrl: './hakkimizda.component.css'
+  styleUrl: './hakkimizda.component.css',
+  imports: [ReactiveFormsModule, FormsModule ],
+  
 })
 export class HakkimizdaComponent {
-
 appointment = {
-    customerId: '',
-    appointmentDate: '',
-    appointmentReason: '',
-    appointmentStatus: '',
-    doctorId: ''
+    customerId: null,
+    appointmentDate: null,
+    appointmentReason: null,
+    appointmentStatus: null,
+    doctorId: null
   };
-
-onSubmit() {
-
-  console.log("Form gönderildi.");
-}
+ name = new FormControl('');
+  onSubmit() {
+    // Form gönderildiğinde yapılacak işlemler
+    console.log('Form gönderildi:', this.appointment);
+  }
 }
